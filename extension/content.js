8cdb51e1fa3c9b88db603fed76a8dc4f482c5294
田生 <tiansheng111@gmail.com>
2020-08-03 19:20:49 +1000

    set async to false on script injected in content script

--- extension/content.js
+++ extension/content.js
@@ -3,6 +3,7 @@
   const optionsPromise = browser.storage.sync.get('btpOptions');
   const script = document.createElement('script');
   script.src = browser.runtime.getURL('page.js');
+  script.async = false;
   script.addEventListener('load', async function () {
     script.remove();
     const options = (await optionsPromise).btpOptions || {};
